b098a6aad4a01085cd4de5b668f00de67a2d2914
Ismail Shpati <ilishpati@gmail.com>
2022-03-04 20:51:06 +0100

    Removed unnecessary file.

--- Randomizer/application/core/PROVA.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$basePath = "/Randomizer/public";
-$queryString = "/Randomizer/public/home/login";
-$removedHome = str_replace($basePath, '', $queryString);
-echo $removedHome . ' ; ORIGINAL: ' . $queryString;
-
-$split = explode('/', trim(str_replace($basePath, '', $queryString), '/'));
-var_dump(explode('/', trim(str_replace($basePath, '', $queryString), '/')));
